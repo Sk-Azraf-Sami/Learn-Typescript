118a7b493df433fc7026266482bd072506abd8a8
Sk-Azraf-Sami <azraf7026official@gmail.com>
2025-05-12 10:02:00 +0600

    utility

--- Lesson-01/src/utility.ts
+++ Lesson-01/src/utility.ts
@@ -66,3 +66,70 @@ const gradeData: Record<studentName, Grades> = {
 }
 
 /****************** Pick & Omit *******************/
+type AssignmentResult = Pick<Assignment, "studentId" | "grade">
+const score: AssignmentResult = {
+    studentId: "ntm",
+    grade: 100
+}
+
+type AssignmentPreview = Omit<Assignment, "grade" | "verified">
+const preview: AssignmentPreview = {
+    studentId: "ntm", 
+    title: "Test Project"
+}
+
+/****************** Exclude & Extract *******************/
+type adjustGrades = Exclude<LetterGrades, "U"> // represents "A", "B", "C", "D" 
+type highGrades = Extract<LetterGrades, "A" | "B"> // represents "A", "B"
+
+/****************** NonNullable *******************/
+type AllPossibleGrades = "ntm" | "sas" | null | undefined; 
+type NamesOnly = NonNullable<AllPossibleGrades> // represents "ntm" | "sas" 
+
+/****************** ReturnType *******************/
+type newAssign = {title: string, point: number}
+const createNewAssign = (title: string, point: number): newAssign => {
+    return {title, point};
+}
+
+type newAssign2 = ReturnType<typeof createNewAssign> 
+const tAssign: newAssign2 = createNewAssign("Utility Types", 100)
+console.log(tAssign);
+
+
+/****************** Parameters *******************/
+type AssignParams = Parameters<typeof createNewAssign> 
+const assignArgs: AssignParams = ["Image Processing Project", 100]; 
+const tAssign2: newAssign = createNewAssign(...assignArgs);
+console.log(tAssign2); 
+
+/****************** Awaited-> helps with the return type of Promise *******************/
+interface User{
+    id: number, 
+    name: string, 
+    username: string, 
+    email: string 
+}
+
+const fetchUsers = async (): Promise<User[]> => {
+    const data = await fetch('https://jsonplaceholder.typicode.com/users') 
+                            .then(res=>{
+                                return res.json()
+                            })
+                            .catch(err=>{
+                                if(err instanceof Error){
+                                    console.log(err.message)
+                                }
+                            })
+
+                return data; 
+    
+}
+
+// But here return type is Promise 
+// type FetchUsersReturnType = Promise<User[]>
+// But we need Users[] array 
+// type FetchUsersReturnType = ReturnType<typeof fetchUsers>
+
+type FetchUsersReturnType = Awaited<ReturnType<typeof fetchUsers>>
+fetchUsers().then(users => console.log(users)); 
